5473c65c03e5f13da90cfdaff45c3a7dbf2d4f25
thibaudj <thibaud.jacquemot@sncf.fr>
2021-01-11 14:15:50 +0100

    Quête résolue

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.repository;
 
 import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
+import util.JdbcUtils;
+
+import java.sql.*;
 
 public class SchoolRepository {
 
@@ -10,7 +14,42 @@ public class SchoolRepository {
 
     public School save(String name, Long capacity, String country) {
 
-        // TODO : insert a new school in database
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet generatedKeys = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+
+            statement = connection.prepareStatement(
+                    "INSERT INTO school (name, capacity, country) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            statement.setString(1, name);
+            statement.setLong(2, capacity);
+            statement.setString(3, country);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to insert data");
+            }
+
+            generatedKeys = statement.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getLong(1);
+                return new School(id, name, capacity, country);
+            } else {
+                throw new SQLException("failed to get inserted id");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeResultSet(generatedKeys);
+            JdbcUtils.closeStatement(statement);
+            JdbcUtils.closeConnection(connection);
+        }
         return null;
     }
 }
